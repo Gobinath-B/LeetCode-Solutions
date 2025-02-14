56e18c1371205ce3fd457533ada9e083514cdc8f
Gobinath B <princegopinath42@gmail.com>
2025-02-14 22:04:15 +0530

    Time: 1 ms (36.77%), Space: 41.6 MB (57.53%) - LeetHub

---
+++ 0953-reverse-only-letters/0953-reverse-only-letters.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        int l=0;
+        int r= s.length()-1;
+        char[] res = new char[s.length()];
+        while(l<=r){
+        if(Character.isAlphabetic(s.charAt(l)) && Character.isAlphabetic(s.charAt(r))){
+            res[l] = s.charAt(r);
+            res[r] = s.charAt(l);
+            l++;
+            r--;
+        }
+        else if(!Character.isAlphabetic(s.charAt(l))){
+            res[l]= s.charAt(l);
+            l++;
+        }else if(!Character.isAlphabetic(s.charAt(r))){
+            res[r]= s.charAt(r);
+            r--;
+        }
+        }
+       
+        return new String(res);
+    }
+}

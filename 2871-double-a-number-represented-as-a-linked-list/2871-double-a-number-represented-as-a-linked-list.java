2dee6630cfbc2774ad6f80d57bba6c792b0d0370
Gobinath B <princegopinath42@gmail.com>
2025-04-06 14:01:54 +0530

    Time: 159 ms (5.1%), Space: 46.7 MB (60.28%) - LeetHub

--- 2871-double-a-number-represented-as-a-linked-list/2871-double-a-number-represented-as-a-linked-list.java
+++ 2871-double-a-number-represented-as-a-linked-list/2871-double-a-number-represented-as-a-linked-list.java
@@ -6,17 +6,14 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         ListNode temp = head;
 
-        // Step 1: Build the number string
         while (temp != null) {
             sb.append(temp.val);
             temp = temp.next;
         }
 
-        // Step 2: Convert to BigInteger and double it
         BigInteger num = new BigInteger(sb.toString());
         num = num.multiply(BigInteger.valueOf(2));
 
-        // Step 3: Convert back to list
         String doubledStr = num.toString();
         ListNode dummy = new ListNode(0);
         temp = dummy;
